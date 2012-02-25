ef34e9169bc16eb328b5a887ac09aa37aeba62e0
Lila Stuti <stuti.lila@gmail.com>
2012-02-24 23:06:44 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uwi::Application.config.secret_token = 'a605095ee079bdf07250ffb3df1ab60218c9316796502f29573ee4f8eda2a0f346de5ccf3f719d32d699cc9b3b87b8089a5f5d4ca68f183635952c86e4acdb13'
