9d6530c80b6e0b81676d11f245d97afadd1030ac
SatiDaer <sati.daer@gmail.com>
2012-02-27 02:31:28 -0300

    Added a Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Uwi::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/help"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
